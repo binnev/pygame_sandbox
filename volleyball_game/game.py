d2523d91e49e06ebc5866a757891922cb54ba800
binnev <byntje@gmail.com>
2020-09-20 16:42:42 +0200

    Simplified it, I guess.

--- volleyball_game/game.py
+++ volleyball_game/game.py
@@ -33,13 +33,13 @@ class VolleyballGame(Game):
         # input devices that are specific to this game
         self.keyboard0 = Keyboard0()
         self.keyboard1 = Keyboard1()
-        self.controller0 = GamecubeController(controller_id=0)
-        self.controller1 = GamecubeController(controller_id=1)
+        # self.controller0 = GamecubeController(controller_id=0)
+        # self.controller1 = GamecubeController(controller_id=1)
         self.input_devices += [
             self.keyboard0,
             self.keyboard1,
-            self.controller0,
-            self.controller1,
+            # self.controller0,
+            # self.controller1,
         ]
 
     def run(self):
@@ -52,8 +52,8 @@ class VolleyballGame(Game):
             (3 * conf.SCREEN_WIDTH // 4, conf.SCREEN_HEIGHT - 100),
         ]
 
-        player1 = Stickman(*starting_positions[0], input=self.controller0, facing_right=True,)
-        player2 = Stickman(*starting_positions[1], input=self.controller1, facing_right=False,)
+        player1 = Stickman(*starting_positions[0], input=self.keyboard0, facing_right=True,)
+        player2 = Stickman(*starting_positions[1], input=self.keyboard1, facing_right=False,)
         level.add(
             player1, player2, type="character",
         )
@@ -86,10 +86,11 @@ class VolleyballGame(Game):
 
         def tee_up(level, match, player1, player2):
             # tee up the ball for whoever's turn it is to serve
-            reset()
-            x = player1.x if match.to_serve == "left" else player2.x
-            level.add(Volleyball(x, 100), type="projectile")
-            match.ball_in_play = True
+            if not match.ball_in_play and match.game_started:
+                reset()
+                x = player1.x if match.to_serve == "left" else player2.x
+                level.add(Volleyball(x, 100), type="projectile")
+                match.ball_in_play = True
 
         def score_balls_out_of_play(level, match):
             # do scoring if ball falls off bottom
@@ -138,11 +139,6 @@ class VolleyballGame(Game):
                 match.score = [0, 0]
                 reset()
 
-        run = True
-        debug = False
-        frame_by_frame = False
-
-        # fixme: this is ugly and hacky
         class Match:
             score = [0, 0]
             to_serve = "left"
@@ -150,9 +146,13 @@ class VolleyballGame(Game):
             game_started = False
             max_score = 10
 
+        run = True
+        debug = False
+        frame_by_frame = False
+        match = Match()
+
         # main volleyball game loop:
         while run:
-            # todo: duplicated
             for device in self.input_devices:
                 device.read_new_inputs()
             self.window.fill((255, 255, 255))  # clear screen
@@ -170,33 +170,26 @@ class VolleyballGame(Game):
                     x, y = pygame.mouse.get_pos()
                     if button[0]:
                         level.add(PersistentHitbox(x, y), type="particle_effect")
-                    # if button[-1]:
-                    #     level.add(Bowlingball(x, y), type="projectile")
 
             if self.keyboard.is_pressed(pygame.K_F1):
                 debug = not debug
             if self.keyboard.is_pressed(pygame.K_RETURN):
-                Match.game_started = not Match.game_started
+                match.game_started = not match.game_started
             if self.keyboard.is_pressed(pygame.K_F2):
                 frame_by_frame = not frame_by_frame
-            if self.keyboard.is_pressed(pygame.K_b):
-                level.add(
-                    Volleyball(conf.SCREEN_WIDTH // 4, conf.SCREEN_HEIGHT // 4), type="projectile"
-                )
             if self.keyboard.is_down(pygame.K_r):
                 transparent_red = (*pygame.color.THECOLORS["red"][:3], 150)
                 draw.rect(self.window, transparent_red, (200, 100, 60, 200))
                 # we draw to canvas using standard pygame drawing methods
                 pygame.draw.rect(Canvas.get_instance(), transparent_red, (100, 100, 60, 200))
 
-            if not Match.ball_in_play and Match.game_started:
-                tee_up(level, Match, player1, player2)
-            score_bouncing_balls(level, Match)
-            score_balls_out_of_play(level, Match)
-            score_balls_in_net(level, Match)
-            end_game(level, Match)
+            tee_up(level, match, player1, player2)
+            score_bouncing_balls(level, match)
+            score_balls_out_of_play(level, match)
+            score_balls_in_net(level, match)
+            end_game(level, match)
 
-            # wait for button press before advancing
+            # frame-by-frame mode
             if frame_by_frame:
                 if not self.keyboard.is_pressed(pygame.K_F3):
                     continue  # skip the rest of the actions
@@ -209,13 +202,12 @@ class VolleyballGame(Game):
             # draw stuff
             level.draw(self.window, debug=debug)
             # draw scoreboard
-            text = self.font.render(f"{Match.score[0]}-{Match.score[1]}", True, (0, 0, 0))
+            text = self.font.render(f"{match.score[0]}-{match.score[1]}", True, (0, 0, 0))
             textRect = text.get_rect()
             textRect.center = (self.window_width // 2, 50)
             self.window.blit(text, textRect)
 
             # Canvas draws its contents to screen once per tick.
-            # Canvas.blit_to_window()
             pygame.display.flip()
 
             # destroy hitboxes
