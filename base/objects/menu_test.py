7b31bc93e5845c669c509b95be4c33d85722dfd9
binnev <byntje@gmail.com>
2020-09-23 08:58:02 +0200

    Buttons have text and do stuff. You can't assign things in lambda functions which is quite a handicap for callbacks that need to change the state of an object... And quite unpythonic that they WOULD allow you to call a .set_state() method. Oh well. Easing animations also working nicely.

--- base/objects/menu_test.py
+++ base/objects/menu_test.py
@@ -1,9 +1,13 @@
 import sys
+from random import random
 
 import numpy
 import pygame
+from pygame import Color
 
+from base.animation import ease_in_out
 from base.groups import EntityGroup
+from base.objects.entities import Entity
 from base.objects.gui_elements import GuiButton
 from base.objects.gui_test import mouse_hovering_over, mouse_clicking
 from base.objects.mixins import AnimationMixin
@@ -11,29 +15,16 @@ from base.objects.mixins import AnimationMixin
 mainClock = pygame.time.Clock()
 pygame.init()
 pygame.display.set_caption("Game base")
-screen = pygame.display.set_mode((500, 500), 0, 32)
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 500
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
 font = pygame.font.SysFont(None, 50)
-
-
-def animate_ease(start, stop, num, function):
-    distance = stop - start
-    ease = numpy.array(list(map(function, numpy.linspace(0, 1, num))))
-    output = start + ease * distance
-    return output
-
-
-def ease_in(start, stop, num, power=3):
-    return animate_ease(start, stop, num, function=lambda x: x ** power)
-
-
-def ease_out(start, stop, num, power=3):
-    return animate_ease(start, stop, num, function=lambda x: 1 - (1 - x) ** power)
 
 
 class Menu(EntityGroup, AnimationMixin):
     def update(self, *args):
         super().update()  # call .update() on all children objects in self
-        self.ticks_elapsed += 1
+        self.update_animation()
         self.state()
         self.check_children_status()
 
@@ -43,31 +34,75 @@ class Menu(EntityGroup, AnimationMixin):
             element.click = mouse_clicking(element)
 
 
+class Explosion(Entity, AnimationMixin):
+    def __init__(self, x, y):
+        self.original_position = x, y
+        size = 5
+        super().__init__(x, y, size, size)
+
+    def update(self):
+        self.update_animation()
+        self.rect.height *= 2
+        self.rect.width *= 2
+        self.xy = self.original_position
+        if self.ticks_elapsed == 6:
+            self.kill()
+
+    def draw(self, surface, debug=False):
+        pygame.draw.ellipse(surface, Color("orange"), self.rect)
+
+
 class MainMenu(Menu):
     def __init__(self):
         super().__init__()
         self.state = self.animate_in
-        self.button1 = GuiButton(0, 0, 200, 50)
-        self.button2 = GuiButton(0, 0, 200, 50)
+        self.button1 = GuiButton(
+            0,
+            200,
+            200,
+            50,
+            text="random explosion",
+            text_color=Color("white"),
+            on_click=lambda: self.add(
+                Explosion(x=random() * SCREEN_WIDTH, y=random() * SCREEN_HEIGHT)
+            ),
+        )
+
+        def goto_state(new_state):
+            self.state = new_state
+
+        self.button2 = GuiButton(
+            0,
+            300,
+            200,
+            50,
+            text="quit",
+            text_color=Color("white"),
+            on_click=lambda: goto_state(self.animate_out),
+        )
         self.add(self.button1, self.button2)
 
     def animate_in(self):
         try:
             # todo: maybe make these generators so that you're not generating the entire array
             #  each tick
-            xs = ease_out(0, 250, 30)
-            ys = ease_out(0, 200, 30)
+            xs = ease_in_out(-250, 250, 60)
             self.button1.x = xs[self.ticks_elapsed]
-            self.button1.y = ys[self.ticks_elapsed]
-
-            ys = ease_out(0, 300, 30)
             self.button2.x = xs[self.ticks_elapsed]
-            self.button2.y = ys[self.ticks_elapsed]
         except IndexError:
             self.state = self.idle
 
     def idle(self):
         pass  # waiting for user input here.
+
+    def animate_out(self):
+        try:
+            xs = ease_in_out(250, 750, 60)
+            self.button1.x = xs[self.ticks_elapsed]
+            self.button2.x = xs[self.ticks_elapsed]
+        except IndexError:
+            pygame.quit()
+            sys.exit()
 
 
 def draw_text(text, font, color, surface, x, y):
