6760feb7eda482a3ab8e25621a9b6e5aa7dea230
binnev <byntje@gmail.com>
2020-10-02 17:08:41 +0200

    Can now start the game via the main menu!

--- volleyball_game/game.py
+++ volleyball_game/game.py
@@ -1,23 +1,25 @@
 import sys
 import time
+from random import random
 
 import pygame
+from pygame import Color
 from pygame.sprite import Sprite
 
-from base import draw
+from base.animation import ease_in_out
 from base.draw import Canvas
 from base.game import Game
 from base.groups import EntityGroup
-from base.objects.entities import Platform
-from base.objects.menu_test import MainMenu
+from base.objects.gui_elements import GuiButton
+from base.objects.menu_test import Menu
 from volleyball_game import conf
 from volleyball_game.inputs import GamecubeController, Keyboard1, Keyboard0
 from volleyball_game.levels import VolleyballCourt
+from volleyball_game.objects import ParticleEffect
 from volleyball_game.objects import (
     Stickman,
     Volleyball,
     HitHandler,
-    PersistentHitbox,
 )
 
 
@@ -54,7 +56,7 @@ class VolleyballGame(Game):
     def main(self):
         """ This is the outermost game function which runs once. It contains the outermost game
         loop. Here's where you should put your main event state machine. """
-        self.add_scene(VolleyballMatch())
+        self.add_scene(MainMenu())
         self.debug = False
         self.tick = 0
         running = True
@@ -77,6 +79,13 @@ class VolleyballGame(Game):
             for device in self.input_devices:
                 device.read_new_inputs()
 
+            # # frame-by-frame mode
+            # if self.frame_by_frame:
+            #     if not self.game.keyboard.is_pressed(pygame.K_F3):
+            #         return  # skip the rest of the actions
+            #     else:
+            #         print("advanced 1 frame")
+
             self.scenes.update()
             self.scenes.draw(self.window, debug=self.debug)
             pygame.display.update()
@@ -86,186 +95,11 @@ class VolleyballGame(Game):
             if not self.scenes:
                 running = False
 
-    def run(self):
-
-        # setup stuff before the main game loop:
-        self.window.fill((255, 255, 255))
-        level = VolleyballCourt(game=self)
-        starting_positions = [
-            (conf.SCREEN_WIDTH // 4, conf.SCREEN_HEIGHT - 100),
-            (3 * conf.SCREEN_WIDTH // 4, conf.SCREEN_HEIGHT - 100),
-        ]
-
-        player1 = Stickman(*starting_positions[0], input=self.controller0, facing_right=True,)
-        player2 = Stickman(*starting_positions[1], input=self.controller1, facing_right=False,)
-        level.add(
-            player1, player2, type="character",
-        )
-        hit_handler = HitHandler()
-
-        def reset():
-            player1.xy = starting_positions[0]
-            player1.u = player1.v = 0
-            player1.facing_right = True
-            player1.state = player1.state_stand
-            player2.xy = starting_positions[1]
-            player2.u = player2.v = 0
-            player2.facing_right = False
-            player2.state = player2.state_stand
-
-        def score_bouncing_balls(level, match):
-            """ do scoring if ball bounces off ground. """
-            bouncing_balls = pygame.sprite.spritecollide(
-                level.ground, level.projectiles, dokill=False
-            )
-            for ball in bouncing_balls:
-                time.sleep(1)
-                # right player goal
-                if ball.x < level.net.x:
-                    match.score[-1] += 1
-                    match.to_serve = "left"
-                else:
-                    match.score[0] += 1
-                    match.to_serve = "right"
-                ball.kill()
-                match.ball_in_play = False
-
-        def tee_up(level, match, player1, player2):
-            # tee up the ball for whoever's turn it is to serve
-            if not match.ball_in_play and match.game_started:
-                reset()
-                x = player1.x if match.to_serve == "left" else player2.x
-                level.add(Volleyball(x, 100), type="projectile")
-                match.ball_in_play = True
-
-        def score_balls_out_of_play(level, match):
-            # do scoring if ball falls off bottom
-            for ball in level.projectiles:
-                if ball.rect.bottom > conf.SCREEN_HEIGHT:
-                    # player 1 knocked the ball out
-                    if ball.last_touched_by == player1:
-                        match.score[-1] += 1
-                        match.to_serve = "left"
-                    else:
-                        match.score[0] += 1
-                        match.to_serve = "right"
-                    time.sleep(1)
-                    match.ball_in_play = False
-                    ball.kill()
-
-        def score_balls_in_net(level, match):
-            # do scoring if ball bounces off net
-            bouncing_balls = pygame.sprite.spritecollide(level.net, level.projectiles, dokill=False)
-            for ball in bouncing_balls:
-                # if the ball bounces off the top of the net, ignore it
-                if ball.centroid.y < level.net.rect.top:
-                    continue
-                if ball.last_touched_by == player1:
-                    match.score[-1] += 1
-                    match.to_serve = "left"
-                else:
-                    match.score[0] += 1
-                    match.to_serve = "right"
-                time.sleep(1)
-                match.ball_in_play = False
-                ball.kill()
-
-        def end_game(level, match):
-            # end the game
-            if match.score[0] == match.max_score or match.score[1] == match.max_score:
-                winner = "Left" if match.score[0] == match.max_score else "Right"
-                text = self.font.render(
-                    f"{winner} player wins {match.score[0]}-{match.score[1]}", True, (0, 0, 0)
-                )
-                textRect = text.get_rect()
-                textRect.center = (self.window_width // 2, 50)
-                self.window.blit(text, textRect)
-                pygame.display.flip()
-                time.sleep(3)
-                match.score = [0, 0]
-                reset()
-
-        class Match:
-            score = [0, 0]
-            to_serve = "left"
-            ball_in_play = False
-            game_started = False
-            max_score = 10
-
-        run = True
-        debug = False
-        frame_by_frame = False
-        match = Match()
-
-        # main volleyball game loop:
-        while run:
-            for device in self.input_devices:
-                device.read_new_inputs()
-            self.window.fill((255, 255, 255))  # clear screen
-            Canvas.get_instance().fill((255, 255, 255))
-
-            if self.keyboard.is_pressed(pygame.K_ESCAPE):
-                run = False
-                time.sleep(1)
-
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    button = pygame.mouse.get_pressed()
-                    x, y = pygame.mouse.get_pos()
-                    if button[0]:
-                        level.add(PersistentHitbox(x, y), type="particle_effect")
-
-            if self.keyboard.is_pressed(pygame.K_F1):
-                debug = not debug
-            if self.keyboard.is_pressed(pygame.K_RETURN):
-                match.game_started = not match.game_started
-            if self.keyboard.is_pressed(pygame.K_F2):
-                frame_by_frame = not frame_by_frame
-            if self.keyboard.is_down(pygame.K_r):
-                transparent_red = (*pygame.color.THECOLORS["red"][:3], 150)
-                draw.rect(self.window, transparent_red, (200, 100, 60, 200))
-                # we draw to canvas using standard pygame drawing methods
-                pygame.draw.rect(Canvas.get_instance(), transparent_red, (100, 100, 60, 200))
-
-            tee_up(level, match, player1, player2)
-            score_bouncing_balls(level, match)
-            score_balls_out_of_play(level, match)
-            score_balls_in_net(level, match)
-            end_game(level, match)
-
-            # frame-by-frame mode
-            if frame_by_frame:
-                if not self.keyboard.is_pressed(pygame.K_F3):
-                    continue  # skip the rest of the actions
-                else:
-                    print("advanced 1 frame")
-
-            level.update()
-            hit_handler.handle_hits(level.hitboxes, [*level.characters, *level.projectiles])
-
-            # draw stuff
-            level.draw(self.window, debug=debug)
-            # draw scoreboard
-            text = self.font.render(f"{match.score[0]}-{match.score[1]}", True, (0, 0, 0))
-            textRect = text.get_rect()
-            textRect.center = (self.window_width // 2, 50)
-            self.window.blit(text, textRect)
-
-            # Canvas draws its contents to screen once per tick.
-            pygame.display.flip()
-
-            # destroy hitboxes
-            level.hitboxes.kill()
-
-            self.clock.tick(self.fps)
-
 
 class VolleyballMatch(Sprite):
     """ The scene in which the volleyballmatch takes place. """
 
-    game: VolleyballGame  # added by the game it adds this scene
+    game: VolleyballGame  # added by the game when it adds this scene
     starting_positions = [
         (conf.SCREEN_WIDTH // 4, conf.SCREEN_HEIGHT - 100),
         (3 * conf.SCREEN_WIDTH // 4, conf.SCREEN_HEIGHT - 100),
@@ -293,15 +127,15 @@ class VolleyballMatch(Sprite):
         self.groups = [self.level]
         self.frame_by_frame = False
         self.hit_handler = HitHandler()
-
-    def reset_match(self):
         self.score = [0, 0]
         self.to_serve = "left"
         self.ball_in_play = False
         self.game_started = False
         self.max_score = 10
-        self.reset_player_positions()
-        self.level.projectiles.kill()
+
+    def reset_match(self):
+        self.game.add_scene(VolleyballMatch())
+        self.kill()
 
     def start_match(self):
         """ Stuff that can't go in the init method (probably because it refers to self.game
@@ -310,10 +144,9 @@ class VolleyballMatch(Sprite):
             *self.starting_positions[0], input=self.game.controller0, facing_right=True,
         )
         self.player2 = Stickman(
-            *self.starting_positions[1], input=self.game.keyboard1, facing_right=False,
+            *self.starting_positions[1], input=self.game.controller1, facing_right=False,
         )
         self.level.add(self.player1, self.player2, type="character")
-        self.reset_match()
         self.state = self.match
 
     def match(self):
@@ -337,13 +170,6 @@ class VolleyballMatch(Sprite):
         self.score_balls_out_of_play()
         # self.score_balls_in_net()
         self.end_game()
-
-        # # frame-by-frame mode
-        # if self.frame_by_frame:
-        #     if not self.game.keyboard.is_pressed(pygame.K_F3):
-        #         return  # skip the rest of the actions
-        #     else:
-        #         print("advanced 1 frame")
 
         self.hit_handler.handle_hits(
             self.level.hitboxes, [*self.level.characters, *self.level.projectiles]
@@ -435,3 +261,91 @@ class VolleyballMatch(Sprite):
             time.sleep(3)
             self.score = [0, 0]
             self.reset_player_positions()
+
+
+class MainMenu(Menu):
+    animation_time = 40
+
+    def __init__(self):
+        super().__init__()
+        self.state = self.animate_in
+        self.play_button = GuiButton(
+            -999, 200, 200, 50, text="PLAY!", text_color=Color("white"), color=(100, 0, 100),
+        )
+        self.quit_button = GuiButton(
+            -999, 400, 200, 50, text="quit", text_color=Color("white"), color=(100, 0, 100),
+        )
+        self.settings_button = GuiButton(
+            -999, 300, 200, 50, text="settings", text_color=Color("white"), color=(100, 0, 100),
+        )
+        self.buttons.add(self.play_button, self.quit_button, self.settings_button)
+        self.explosions = EntityGroup()
+        self.groups.append(self.explosions)
+
+    def animate_in(self):
+        try:
+            # todo: maybe make these generators so that you're not generating the entire array
+            #  each tick
+            xs = ease_in_out(-250, conf.SCREEN_WIDTH // 2, self.animation_time)
+            for button in self.buttons:
+                button.x = xs[self.game_tick]
+        except IndexError:
+            self.state = self.idle
+
+    def idle(self):
+        if self.quit_button.click:
+            self.state = self.animate_to(self.animate_out, None)
+        if self.settings_button.click:
+            self.game.add_scene(SettingsMenu())
+            self.state = self.animate_to(self.animate_out, None)
+        if self.play_button.click:
+            self.game.add_scene(VolleyballMatch())
+            self.state = self.animate_to(self.animate_out, None)
+
+            for ii in range(30):
+                self.explosions.add(
+                    ParticleEffect(x=random() * conf.SCREEN_WIDTH, y=random() * conf.SCREEN_HEIGHT)
+                ),
+
+    def animate_out(self, next_scene):
+        try:
+            xs = ease_in_out(conf.SCREEN_WIDTH // 2, conf.SCREEN_WIDTH + 250, self.animation_time)
+            for button in self.buttons:
+                button.x = xs[self.game_tick]
+        except IndexError:
+            if next_scene:
+                self.game.scenes.add(next_scene)
+            self.kill()
+
+
+class SettingsMenu(Menu):
+    animation_time = 40
+
+    def __init__(self):
+        super().__init__()
+        self.state = self.animate_in
+        self.back_button = GuiButton(
+            -999, 200, 200, 50, text="back", text_color=Color("white"), color=(100, 0, 100),
+        )
+        self.buttons.add(self.back_button)
+
+    def animate_in(self):
+        try:
+            xs = ease_in_out(-250, conf.SCREEN_WIDTH // 2, self.animation_time)
+            self.back_button.x = xs[self.game_tick]
+        except IndexError:
+            self.state = self.idle
+
+    def idle(self):
+        if self.back_button.click:
+            self.game.add_scene(MainMenu())
+            self.state = self.animate_to(self.animate_out, None)
+
+    def animate_out(self, next_scene):
+        try:
+            xs = ease_in_out(conf.SCREEN_WIDTH // 2, conf.SCREEN_WIDTH + 250, self.animation_time)
+            self.back_button.x = xs[self.game_tick]
+        except IndexError:
+            if next_scene:
+                self.game.scenes.add(next_scene)
+            self.kill()
