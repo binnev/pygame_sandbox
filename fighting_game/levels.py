2036f2aacbca1e41a96b662555f671a683a9c6a1
binnev <byntje@gmail.com>
2020-10-18 21:04:35 +0200

    Added more platforms.

--- fighting_game/levels.py
+++ fighting_game/levels.py
@@ -17,6 +17,22 @@ class DefaultLevel(Level):
         self.net.x = SCREEN_WIDTH // 2
         self.net.y = SCREEN_HEIGHT - 300
 
-        self.droppable = Platform(500, 500, 300, 40, droppable=True)
+        self.net2 = Platform(0, 0, 10, NET_HEIGHT // 4)
+        self.net2.x = SCREEN_WIDTH // 2
+        self.net2.y = SCREEN_HEIGHT - 75
 
-        self.add(self.ground, self.net, self.droppable, type="platform")
+        self.droppable = Platform(SCREEN_WIDTH // 4, 500, 300, 40, droppable=True)
+        self.droppable2 = Platform(SCREEN_WIDTH // 4, 400, 300, 20, droppable=True)
+        self.droppable3 = Platform(SCREEN_WIDTH * 3 // 4, 500, 300, 40, droppable=True)
+        self.droppable4 = Platform(SCREEN_WIDTH * 3 // 4, 400, 300, 20, droppable=True)
+
+        self.add(
+            self.ground,
+            self.net,
+            self.net2,
+            self.droppable,
+            self.droppable2,
+            self.droppable3,
+            self.droppable4,
+            type="platform",
+        )
