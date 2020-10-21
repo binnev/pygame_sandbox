38cfb8825f8d79034c1be186b186916c1686b8d0
binnev <byntje@gmail.com>
2020-10-21 16:46:46 +0200

    Got some basic shiz working

--- blitting_flags_test/__main__.py
+++ blitting_flags_test/__main__.py
@@ -16,6 +16,7 @@ def circle_surf(radius, color):
 def main():
     pygame.init()
     pygame.display.init()
+    clock = pygame.time.Clock()
     window = pygame.display.set_mode([1600, 900])
     foreground = Surface(window.get_size())
     background = Surface(window.get_size())
@@ -27,6 +28,7 @@ def main():
     while run:
         window.fill((0, 0, 0, 0))
 
+        space_pressed = False
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -35,23 +37,27 @@ def main():
                 if event.key == pygame.K_ESCAPE:
                     pygame.quit()
                     sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                x, y = pygame.mouse.get_pos()
-                left, middle, right = pygame.mouse.get_pressed()
-                # currently drawing with full alpha onto fg/bg surfaces. Need to also use
-                # blitting here if I want to have transparency between objects on the same layer.
-                if left:
-                    circle = circle_surf(50, (100, 50, 69, 100))
-                    foreground.blit(
-                        circle, (x, y), special_flags=pygame.BLEND_RGB_ADD,
-                    )
-                if right:
-                    pygame.draw.rect(background, (0, 30, 50), (x, y, 100, 100))
+                if event.key == pygame.K_SPACE:
+                    space_pressed = True
+
+        x, y = pygame.mouse.get_pos()
+        left, middle, right = pygame.mouse.get_pressed()
+        space_down = pygame.key.get_pressed()[pygame.K_SPACE]
+        # currently drawing with full alpha onto fg/bg surfaces. Need to also use
+        # blitting here if I want to have transparency between objects on the same layer.
+        if space_pressed:
+            circle = circle_surf(50, (50, 25, 33))
+            foreground.blit(
+                circle, (x, y), special_flags=pygame.BLEND_RGB_ADD,
+            )
+        if space_down:
+            pygame.draw.rect(background, (20, 30, 50), (x, y, 100, 100))
 
         # draw stuff
         window.blit(background, background.get_rect())  # todo: flags
         window.blit(foreground, foreground.get_rect(), special_flags=pygame.BLEND_RGB_ADD)
         pygame.display.update()
+        clock.tick(20)
 
 
 if __name__ == "__main__":
