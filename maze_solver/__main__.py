c54acfa4b16e84019f3c9ef32265619197f10122
binnev <byntje@gmail.com>
2021-11-29 17:49:59 +0100

    Added game class for maze solver

--- maze_solver/__main__.py
+++ maze_solver/__main__.py
@@ -1,71 +1,16 @@
-"""
-░▒▓█
-"""
 import sys
 
 import pygame
 
-from maze_solver.objects import Maze
-from maze_solver.test_mazes import MAZES
+from maze_solver.game import MazeSolverGame
 
 
-def path_finder(maze):
-    m = Maze(maze)
-    return m.can_find_path()
+def main():
+    game = MazeSolverGame()
+    game.main()
+    pygame.quit()
+    sys.exit()
 
 
-max_size = [0]
-
-fps = 60
-window_width = 1000
-window_height = 1000
-window_caption = "Maze Solver"
-font_name = "ubuntu"
-font_size = 20
-ticks_per_frame = 10  # how many iterations to do between draws
-
-pygame.init()
-pygame.font.init()
-font = pygame.font.Font(pygame.font.match_font(font_name), font_size)
-window = pygame.display.set_mode((window_width, window_height))
-pygame.display.set_caption(window_caption)
-clock = pygame.time.Clock()
-
-for inp, solvable in MAZES:
-    inp = "\n".join(inp)
-    maze = Maze(inp)
-    running = True
-    debug = False
-    tick = 0
-    while running:
-        events = pygame.event.get()
-        for event in events:
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    pygame.quit()
-                    sys.exit()
-                if event.key == pygame.K_F1:
-                    debug = not debug
-                if event.key == pygame.K_SPACE:
-                    running = False
-                if event.key == pygame.K_DOWN:
-                    fps = max([1, fps // 2])
-                    print(f"fps: {fps}")
-                if event.key == pygame.K_UP:
-                    fps *= 2
-                    print(f"fps: {fps}")
-
-        maze.update()
-
-        if tick % ticks_per_frame == 0:
-            window.fill(pygame.Color("white"))  # clear the screen
-            maze.draw(window)
-            pygame.display.update()  # print to screen
-
-        tick += 1
-        # clock.tick(fps)
-
-
+if __name__ == "__main__":
+    main()
---
+++ maze_solver/game.py
@@ -0,0 +1,91 @@
+import sys
+
+import pygame
+from pygame.color import Color
+from pygame.surface import Surface
+
+from fighting_game.objects import Entity, Group
+from maze_solver.objects import Maze
+from maze_solver.test_mazes import MAZES
+
+
+class MazeSolverGame(Entity):
+    fps = 60
+    window_width = 1000
+    window_height = 1000
+    window_caption = "Maze Solver"
+    font_name = "ubuntu"
+    font_size = 20
+    ticks_per_frame = 10  # how many iterations to do between draws
+
+    def __init__(self):
+        super().__init__()
+        pygame.init()
+
+        self.scenes = Group()
+        self.child_groups = [self.scenes]
+
+        pygame.font.init()
+        self.font = pygame.font.Font(pygame.font.match_font(self.font_name), self.font_size)
+        self.window = pygame.display.set_mode((self.window_width, self.window_height))
+        pygame.display.set_caption(self.window_caption)
+        self.clock = pygame.time.Clock()
+
+    def main(self):
+        """This is the outermost game function which runs once. It contains the outermost game
+        loop. Here's where you should put your main event state machine."""
+
+        for inp, solvable in MAZES:
+            inp = "\n".join(inp)
+            self.maze = Maze(inp)
+            self.running = True
+            self.debug = False
+
+            self.add_scene(self.maze)
+            self.debug = False
+            self.running = True
+            while self.running:
+                self.update()
+                self.draw(self.window, debug=self.debug)
+
+    def add_scene(self, *objects):
+        self.add_to_group(*objects, group=self.scenes)
+
+    def update(self):
+        self.events = pygame.event.get()
+        if self.debug:
+            print(self.tick, self.events)
+        for event in self.events:
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+                if event.key == pygame.K_F1:
+                    self.debug = not self.debug
+                if event.key == pygame.K_SPACE:
+                    self.maze.kill()
+                    self.running = False
+                if event.key == pygame.K_DOWN:
+                    self.ticks_per_frame = max([1, self.ticks_per_frame // 2])
+                    print(f"self.ticks_per_frame: {self.ticks_per_frame}")
+                if event.key == pygame.K_UP:
+                    self.ticks_per_frame *= 2
+                    print(f"self.ticks_per_frame: {self.ticks_per_frame}")
+
+        super().update()
+        # self.clock.tick(self.fps)
+
+        # if there are no scenes to play, exit
+        if not self.scenes:
+            self.running = False
+
+    def draw(self, surface: Surface, debug: bool = False):
+        if self.tick % self.ticks_per_frame == 0:
+            surface.fill(Color("white"))  # clear the screen
+            super().draw(surface, debug)
+            text_bitmap = self.font.render(f"iterations: {self.tick}", True, Color("black"))
+            surface.blit(text_bitmap, (0, 0))
+            pygame.display.update()  # print to screen
