c964fa5bb0e0a2da21de7e07495105cbfc2d1e1b
binnev <byntje@gmail.com>
2022-01-31 18:30:35 +0100

    Added piece classes and move properties

--- chess/engine/classes/piece.py
+++ chess/engine/classes/piece.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Tuple
 
 if TYPE_CHECKING:
     from .board import ChessBoard
@@ -11,6 +11,7 @@ class Piece:
     team: str  # "white" or "black"
     letter: str  # "k" for king
     board: "ChessBoard"
+    square: Tuple[int, int]
 
     def __init__(self, team):
         self.team = team
@@ -18,10 +19,78 @@ class Piece:
     def __str__(self):
         return self.letter.upper() if self.team == WHITE else self.letter.lower()
 
+    @property
+    def moves_unfiltered(self):
+        """Set of squares this piece could move to if nothing is blocking the way"""
+        return set()
+
+    @property
+    def moves(self) -> set:
+        """Set of squares this piece could move to, taking into account obstructions and board
+        edges"""
+        return {
+            move
+            for move in self.moves_unfiltered
+            if move in self.board.squares  # respect board limits
+            and (
+                self.board.contents.get(move) is None  # empty squares
+                or self.board.contents.get(move).team != self.team  # only capture enemies
+            )
+        }
+
+    @property
+    def square(self):
+        """Coordinates of self on board"""
+        return self.board.locate(self)
+
 
 class King(Piece):
     letter = "k"
 
+    @property
+    def moves_unfiltered(self):
+        x, y = self.square
+        return {
+            (x - 1, y - 1),
+            (x - 1, y),
+            (x - 1, y + 1),
+            (x, y - 1),
+            (x, y + 1),
+            (x + 1, y - 1),
+            (x + 1, y),
+            (x + 1, y + 1),
+        }
+
 
 class Queen(Piece):
     letter = "q"
+
+
+class Bishop(Piece):
+    letter = "b"
+
+
+class Knight(Piece):
+    letter = "n"
+
+    @property
+    def moves_unfiltered(self) -> set:
+        x, y = self.square
+        return {
+            (x + 1, y + 2),
+            (x + 2, y + 1),
+            (x + 2, y - 1),
+            (x + 1, y - 2),
+            (x - 1, y - 2),
+            (x - 2, y - 1),
+            (x - 2, y + 1),
+            (x - 1, y + 2),
+        }
+
+
+class Rook(Piece):
+    letter = "r"
+
+
+class Pawn(Piece):
+    letter = "p"
--- chess/engine/tests/test_classes/test_piece.py
+++ chess/engine/tests/test_classes/test_piece.py
@@ -1,6 +1,6 @@
 import pytest
 
-from chess.engine.classes import WHITE, King, BLACK, Queen
+from chess.engine.classes import WHITE, King, BLACK, Queen, ChessBoard, Knight, Pawn, Bishop
 
 
 @pytest.mark.parametrize(
@@ -15,3 +15,62 @@ from chess.engine.classes import WHITE, King, BLACK, Queen
 def test_piece_str(piece_class, team, expected_str):
     piece = piece_class(team)
     assert str(piece) == expected_str
+
+
+def test_king_moves():
+    board = ChessBoard(4, 4)
+    king = King(WHITE)
+    board.add_piece(king, (1, 1))
+
+    assert king.moves == {
+        (0, 0),
+        (0, 1),
+        (0, 2),
+        (1, 0),
+        (1, 2),
+        (2, 0),
+        (2, 1),
+        (2, 2),
+    }
+
+
+def test_king_moves_obstructed():
+    board = ChessBoard(4, 4)
+    king = King(WHITE)
+    board.add_piece(king, (0, 0))
+    board.add_piece(Pawn(WHITE), (1, 1))
+    board.add_piece(Bishop(BLACK), (0, 1))
+
+    assert king.moves == {
+        (0, 1),  # enemy bishop
+        (1, 0),  # empty square
+    }
+
+
+def test_knight_moves():
+    board = ChessBoard()
+    knight = Knight(WHITE)
+    board.add_piece(knight, (5, 5))
+    assert knight.moves == {
+        (5 + 1, 5 + 2),
+        (5 + 2, 5 + 1),
+        (5 + 2, 5 - 1),
+        (5 + 1, 5 - 2),
+        (5 - 1, 5 - 2),
+        (5 - 2, 5 - 1),
+        (5 - 2, 5 + 1),
+        (5 - 1, 5 + 2),
+    }
+
+
+def test_knight_moves_obstructed():
+    board = ChessBoard(4, 4)
+    knight = Knight(WHITE)
+    board.add_piece(knight, (0, 1))
+    board.add_piece(Bishop(WHITE), (1, 3))
+    board.add_piece(Pawn(BLACK), (2, 2))
+
+    assert knight.moves == {
+        (2, 0),  # empty square
+        (2, 2),  # enemy pawn
+    }
