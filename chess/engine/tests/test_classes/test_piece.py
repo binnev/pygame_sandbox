04f9f6cbbb4dbbc07519c4d1460a02bc1dd0142e
binnev <byntje@gmail.com>
2022-01-31 19:06:09 +0100

    Added tests for bishop moves

--- chess/engine/tests/test_classes/test_piece.py
+++ chess/engine/tests/test_classes/test_piece.py
@@ -19,7 +19,7 @@ def test_piece_str(piece_class, team, expected_str):
 
 def test_king_moves():
     board = ChessBoard(4, 4)
-    king = King(WHITE)
+    king = King()
     board.add_piece(king, (1, 1))
 
     assert king.moves == {
@@ -49,7 +49,7 @@ def test_king_moves_obstructed():
 
 def test_knight_moves():
     board = ChessBoard()
-    knight = Knight(WHITE)
+    knight = Knight()
     board.add_piece(knight, (5, 5))
     assert knight.moves == {
         (5 + 1, 5 + 2),
@@ -73,4 +73,40 @@ def test_knight_moves_obstructed():
     assert knight.moves == {
         (2, 0),  # empty square
         (2, 2),  # enemy pawn
+    }
+
+
+def test_bishop_moves():
+    board = ChessBoard(5, 5)
+    bishop = Bishop()
+    board.add_piece(bishop, (2, 2))
+    assert bishop.moves == {
+        (0, 0),
+        (1, 1),
+        (3, 3),
+        (4, 4),
+        (0, 4),
+        (1, 3),
+        (3, 1),
+        (4, 0),
+    }
+
+
+def test_bishop_moves_obstructed():
+    board = ChessBoard(5, 5)
+    bishop = Bishop(WHITE)
+    pawn = Pawn(BLACK)
+    king = King(WHITE)
+    board.add_piece(bishop, (2, 2))
+    board.add_piece(pawn, (3, 3))
+    board.add_piece(king, (1, 1))
+    assert bishop.moves == {
+        # (0, 0),  # behind king
+        # (1, 1),  # occupied by king
+        (3, 3),
+        # (4, 4),  # behind pawn
+        (0, 4),
+        (1, 3),
+        (3, 1),
+        (4, 0),
     }
