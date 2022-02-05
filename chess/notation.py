cfe68c5780caa9ae5be7afc0d80988e72087a4b0
binnev <byntje@gmail.com>
2022-02-05 01:00:58 +0100

    Added parse_fen_row.

--- chess/notation.py
+++ chess/notation.py
@@ -1,4 +1,7 @@
+from chess.engine.classes import WHITE, BLACK, Pawn, Rook, Knight, Bishop, Queen, King
 import re
+
+EMPTY = "."
 
 
 def parse_pgn_move(string):
@@ -14,3 +17,53 @@ def parse_pgn_move(string):
     if piece is None:
         piece = "P"  # pawn by default
     return piece, specifier, capture, target
+
+
+def parse_fen_row(string):
+    """
+    E.g. pppp1ppp
+    """
+    pieces = []
+
+    # todo move to pieces module
+    piece_mapping = dict(
+        p=Pawn,
+        r=Rook,
+        n=Knight,
+        b=Bishop,
+        q=Queen,
+        k=King,
+    )
+    for char in string:
+        if char.isnumeric():
+            pieces.extend([EMPTY] * int(char))
+        else:
+            piece_class = piece_mapping[char.lower()]
+            team = WHITE if char.isupper() else BLACK
+            pieces.append(piece_class(team))
+
+    return pieces
+
+
+# def parse_fen_position(string):
+#     """
+#     E.g. standard starting position:
+#     rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1
+#     """
+#     rx = re.compile(
+#         "(\w+/\w+/\w+/\w+/\w+/\w+/\w+/\w+)"  # piece positions from white's POV
+#         " ([wb])?"  # active player (w = white | b = black)
+#         # " (K?Q?k?q?)?"  # castling availability (K = white kingside, q = black queenside)
+#         # " ([\S])?"  # en passant target square (- = standard)
+#         # " (\d+)?"  # Halfmove clock: The number of halfmoves since the last capture or pawn
+#         # # advance, used for the fifty-move rule.
+#         # " (\d+)?"  # Fullmove number: The number of the full move. It starts at 1,
+#         # # and is incremented after Black's move
+#     )
+#     position, *_ = rx.match(string).groups()
+#
+#     rows = position.split("/")
+#     for ii, row in enumerate(rows):
+#         parse_fen_row(row, ii)
+#
+#     return position, _
--- chess/tests/test_notation.py
+++ chess/tests/test_notation.py
@@ -1,6 +1,7 @@
 import pytest
 
-from chess.notation import parse_pgn_move
+from chess.engine.classes import BLACK, Pawn, WHITE, King, Queen
+from chess.notation import parse_pgn_move, parse_fen_position, EMPTY, parse_fen_row
 
 
 @pytest.mark.parametrize(
@@ -22,3 +23,21 @@ def test_parse_pgn_move(string, piece, specifier, capture, target):
     assert c == capture
     assert t == target
 
+
+@pytest.mark.parametrize(
+    "string, expected_pieces",
+    [
+        ("8", [EMPTY] * 8),  # 8 empty spaces,
+        ("p", [Pawn(BLACK)]),
+        ("p2P", [Pawn(BLACK), EMPTY, EMPTY, Pawn(WHITE)]),
+        ("KkQq", [King(WHITE), King(BLACK), Queen(WHITE), Queen(BLACK)]),
+    ],
+)
+def test_parse_fen_row(string, expected_pieces):
+    assert parse_fen_row(string) == expected_pieces
+
+
+# @pytest.mark.parametrize("string", ["8/8/8/8/8/8/8/R7 w KQkq - 0 1"])
+# def test_parse_fen_position(string):
+#     position, _ = parse_fen_position(string)
+#     assert position == "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
