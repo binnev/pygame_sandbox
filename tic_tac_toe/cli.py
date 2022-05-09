ec99d66f5d7294fb95446dcd0131b9b4618f39ec
binnev <byntje@gmail.com>
2022-05-09 22:55:01 +0200

    fixed TTT cli

--- tic_tac_toe/cli.py
+++ tic_tac_toe/cli.py
@@ -1,15 +1,13 @@
-import time
-
 from tic_tac_toe.agent import MinimaxCliAgent, HumanCliAgent
-from tic_tac_toe.constants import X, O
 from tic_tac_toe.controller import CliController
 from tic_tac_toe.match import Match
+from tic_tac_toe.team import TEAM_O, TEAM_X
 
 
 def main():
     controller = CliController(
-        agent_1=MinimaxCliAgent(team=O),
-        agent_2=HumanCliAgent(team=X),
+        agent_1=MinimaxCliAgent(team=TEAM_O),
+        agent_2=HumanCliAgent(team=TEAM_X),
         match=Match(),
     )
     controller.run_match()
