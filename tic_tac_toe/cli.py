acf1d4fe7d97a2c20007c00de73e2dfa617b60d0
binnev <byntje@gmail.com>
2022-05-09 19:13:29 +0200

    removed replay

--- tic_tac_toe/cli.py
+++ tic_tac_toe/cli.py
@@ -14,13 +14,6 @@ def main():
     )
     controller.run_match()
 
-    replay = input("Do you want to replay the match? [y/n]")
-    if replay.lower() == "y":
-        for move, state in controller.match.history:
-            state.print()
-            time.sleep(0.5)
-            print("")
-
 
 if __name__ == "__main__":
     main()
