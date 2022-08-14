72ebd257a8bff9efe43f91494350e06e1431bd5d
binnev <byntje@gmail.com>
2022-08-14 17:25:19 +0200

    Optimized FpsTrackre a bit

--- base/objects/helpers.py
+++ base/objects/helpers.py
@@ -10,18 +10,18 @@ from base.objects.entity import Entity
 
 class FpsTracker(Entity):
     """Displays a little number in the top right of the window if debug=True"""
+    buffer_length = 60
 
     def __init__(self, *groups) -> None:
         super().__init__(*groups)
-        self.queue = deque(maxlen=60)
+        self.queue = deque(maxlen=self.buffer_length)
         self.font = pygame.font.Font(pygame.font.get_default_font(), 12)
 
     def update(self):
         t = time.perf_counter()
         self.queue.append(t)
-        frames = len(self.queue)
         seconds = self.queue[-1] - self.queue[0]
-        self.fps = int(frames / seconds) if (frames and seconds) else 0
+        self.fps = int(self.buffer_length / seconds) if seconds else 0
 
     def draw(self, surface: Surface, debug: bool = False):
         if debug:
