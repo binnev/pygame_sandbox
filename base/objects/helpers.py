b4e754042a6e8148885f53f81cb3e535b748aa02
binnev <byntje@gmail.com>
2022-08-15 12:12:18 +0200

    FpsTracker uses new Font class

--- base/objects/helpers.py
+++ base/objects/helpers.py
@@ -6,16 +6,18 @@ from pygame.color import Color
 from pygame.surface import Surface
 
 from base.objects.entity import Entity
+from base.text.font import fonts
 
 
 class FpsTracker(Entity):
     """Displays a little number in the top right of the window if debug=True"""
+
     buffer_length = 60
+    font = fonts.cellphone_black
 
     def __init__(self, *groups) -> None:
         super().__init__(*groups)
         self.queue = deque(maxlen=self.buffer_length)
-        self.font = pygame.font.Font(pygame.font.get_default_font(), 12)
 
     def update(self):
         t = time.perf_counter()
@@ -25,11 +27,11 @@ class FpsTracker(Entity):
 
     def draw(self, surface: Surface, debug: bool = False):
         if debug:
-            text = self.font.render(f"FPS: {self.fps}", True, Color("black"))
-            text_rect = text.get_rect()
-            text_rect.right = surface.get_rect().right - 10
-            text_rect.top = surface.get_rect().top + 10
-            background = Surface(text_rect.size)
-            background.fill(Color("white"))
-            surface.blit(background, text_rect)
-            surface.blit(text, text_rect)
+            width = 75
+            surf = Surface((width, 15))
+            surf.fill(Color("white"))
+            self.font.render(surf, f"FPS: {self.fps}", wrap=width, align=0, scale=1.5)
+            rect = surf.get_rect()
+            rect.right = surface.get_rect().right - 10
+            rect.top = surface.get_rect().top + 10
+            surface.blit(surf, rect)
