0e013f7d1aac0bacad6720e87d0e4d6cf5dc1681
binnev <byntje@gmail.com>
2022-09-15 10:06:53 +0200

    added pterodactyl

--- dinosaur_jump/game.py
+++ dinosaur_jump/game.py
@@ -4,7 +4,7 @@ from base.objects import Game, Group
 from base.text.font import fonts
 from dinosaur_jump import images
 from dinosaur_jump.objects.background import ScrollingBackground
-from dinosaur_jump.objects.dino import Dino
+from dinosaur_jump.objects.characters import Dino, Ptero
 
 
 class DinoJump(Game):
@@ -22,6 +22,7 @@ class DinoJump(Game):
             self.entities,
         ]
         self.entities.add(Dino(x=100, y=450))
+        self.entities.add(Ptero(x=self.window_width+16-100, y=150))
         self.background.add(ScrollingBackground(0, 250, images.mountains2, speed=5))
         self.background.add(ScrollingBackground(0, 250, images.mountains1, speed=10))
         self.background.add(ScrollingBackground(0, 250, images.grass, speed=20))
--- dinosaur_jump/objects/characters.py
+++ dinosaur_jump/objects/characters.py
@@ -10,7 +10,7 @@ class Dino(PhysicalEntity):
 
     def __init__(self, x, y) -> None:
         super().__init__()
-        self.rect = pygame.rect.Rect(0, 0, 50, 50)
+        self.rect = pygame.rect.Rect(0, 0, 60, 60)
         self.rect.center = (x, y)
         self.state = self.state_run
 
@@ -25,8 +25,19 @@ class Dino(PhysicalEntity):
         if pygame.key.get_pressed()[pygame.K_UP]:
             self.y -= 5
 
-    def update(self):
-        super().update()
 
+class Ptero(PhysicalEntity):
+    frame_duration = 5
 
+    def __init__(self, x, y):
+        super().__init__()
+        self.rect = pygame.rect.Rect(0, 0, 16 * 5, 16 * 5)
+        self.rect.center = (x, y)
+        self.state = self.state_fly
 
+    def state_fly(self):
+        self.image = images.ptero.loop(self.animation_frame)
+        self.x -= 10
+        # if fly off edge of screen; die
+        if self.x < -10:
+            self.kill()
