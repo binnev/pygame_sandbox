ce3b02b5e858a94c90808ea914c5b81722b5afe9
binnev <byntje@gmail.com>
2023-08-03 02:00:41 +0200

    Minimap working

--- automata/gravity/frontend.py
+++ automata/gravity/frontend.py
@@ -40,3 +40,42 @@ class GravityFrontend:
 
     def get_color(self, body):
         return Color("red")
+
+
+class GravityMinimap:
+    def draw(
+        self,
+        surface: Surface,
+        automaton: GravityAutomaton,
+        viewport: FloatRect,
+        debug: bool = False,
+    ):
+        """
+        For now just draw everything
+        """
+        surface.fill(Color("black"))
+
+        # Choose viewport in xy coordinates to filter for visible cells
+        # fit viewport as tightly as possible to world limits
+        world_width, world_height = automaton.contents.size
+        (xmin, xmax), (ymin, ymax) = automaton.contents.limits
+        world_rect_xy = Rect(xmin, ymin, world_width, world_height)
+        image_rect_uv = surface.get_rect()
+        transform = Transform(world_rect_xy, image_rect_uv)
+        viewport_rect_uv = transform.floatrect(viewport)
+
+        # Draw all cells in screen coords
+        for xy, body in automaton.contents.items():
+            color = self.get_color(body)
+            uv = transform.point(xy)
+            radius = body.radius * transform.scale
+            radius = max(radius, 1)
+            pygame.draw.circle(surface, color, center=uv, radius=radius)
+
+        pygame.draw.rect(surface, Color("white"), viewport_rect_uv, 1)
+        if debug:
+            world_rect_uv = transform.rect(world_rect_xy)
+            pygame.draw.rect(surface, Color("yellow"), world_rect_uv, 1)
+
+    def get_color(self, value):
+        return Color("red")
--- automata/gravity/scene.py
+++ automata/gravity/scene.py
@@ -1,10 +1,11 @@
+import numpy
 from pygame import Rect
 from robingame.objects import Entity, Group
 
 from automata.backend import Backend
 from automata.gravity.automaton import GravityAutomaton
 from automata.gravity.body import Body
-from automata.gravity.frontend import GravityFrontend
+from automata.gravity.frontend import GravityFrontend, GravityMinimap
 from automata.input_handler import KeyboardHandler
 from automata.viewer import Viewer
 from automata.viewport_handler import DefaultViewportHandler
@@ -17,7 +18,7 @@ class GravityScene(Entity):
         automaton = GravityAutomaton()
         sun = Body(mass=99999999999999, radius=100)
         automaton.add_body(x=0, y=0, body=sun)
-        for x in range(200, 400, 7):
+        for x in numpy.linspace(150, 500, 100):
             automaton.add_body(
                 x=x,
                 y=0,
@@ -25,23 +26,32 @@ class GravityScene(Entity):
             )
 
         backend = Backend(automaton=automaton)
-        rect = Rect(0, 0, 1000, 1000)
-        viewer = Viewer(
-            rect=rect,
+        main_rect = Rect(0, 0, 1000, 1000)
+        main_map = Viewer(
+            rect=main_rect,
             backend=backend,
             frontend=GravityFrontend(),
             controller=KeyboardHandler(),
             viewport_handler=DefaultViewportHandler(
                 x=0,
                 y=0,
-                width=rect.width,
-                height=rect.height,
+                width=main_rect.width,
+                height=main_rect.height,
             ),
+        )
+        mini_rect = Rect(0, 0, 200, 200)
+        mini_rect.topright = (main_rect.right - 10, main_rect.top + 100)
+        mini_map = Viewer(
+            rect=mini_rect,
+            backend=backend,
+            frontend=GravityMinimap(),
+            viewport_handler=main_map.viewport_handler,
         )
 
         self.children = Group()
         self.child_groups += [self.children]
         self.children.add(
             backend,
-            viewer,
+            main_map,
+            mini_map,
         )
